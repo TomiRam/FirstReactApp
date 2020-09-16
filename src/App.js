4772d8d597e14c846c4c6e191ab8629ee468acf3
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2020-09-16 15:43:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Tweet from "./Tweet";
 
 
 
+
 function App(){
 
   const [users, setUsers]= useState([ 
@@ -10,19 +11,38 @@ function App(){
     {name: 'Escanor', message: 'I am Escanor The one'},
     {name: 'google', message: 'I am awesome'}
   ]);
-  
 
+    const [count, setCount] = useState(0);
+
+  
+    
   return (
 
-    <div className="app">
+    <div className="app" >
      {users.map(user =>(
        <Tweet name={user.name} message={user.message} />
      ))}
-
+      <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+        
+        <form onSubmit={setUsers}>
+          <label >
+            setUsers:
+            
+            <Tweet name="John" message ="Im a developer"/>
+          
+     
+          </label>
+          
+        </form>
+      
     </div>
     );
 
   };
+  
 
 
 export default App;
