0dbfc458c1f1cd24d84b4e844cba70a601b594f0
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2020-09-23 13:49:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import React, {useState} from "react"; 
+import React, {useState } from "react"; 
 import Tweet from "./Tweet";
+
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li stule={{fontSize:"20px"}}>MtnDewJumpstart</li>
+          <li stule={{fontSize:"20px"}}>Nocco Limon del sol</li>
+          <li stule={{fontSize:"20px"}}>MtnDew</li>
+        </ul>
+        
+      </div>
+    );
+  }
+};
 
 
 function App(){
@@ -15,15 +32,20 @@ function App(){
     const GymGoals = ['200kg Deadlift','160kg Squat', '120kg Bench', '100kg Overhead Press', '100kg Barbell Curl', '2min Plank'];
     const listItems = GymGoals.map((gymgoal) =>
     <li>{gymgoal}</li>);
+    
+    
+ 
 
   return (
 
     <div className="app" >
+      <ShoppingList/>
      {users.map(user =>(
        <Tweet name={user.name} message={user.message} />
      ))}
-      <p>You clicked {count} times</p>
-        <button onClick={() => setCount(count + 1)}>
+     
+      <p style={{backgroundColor:"lightblue",fontSize:"25px"}}>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)} style={{backgroundColor:"lightblue"}}>
           Click me
         </button>
         
@@ -32,12 +54,14 @@ function App(){
             setUsers:
             
             <Tweet name="John" message ="Im a developer"/>
-     
+           
           </label>
           
         </form>
-      <h3>My Gym Goals</h3>
-     <ul>{listItems}</ul>
+        <h2 style={{backgroundColor:"lightblue", alignItems: "center", fontSize: "20px"}}>It is {new Date().toLocaleTimeString()}.</h2>
+      <h3 style={{backgroundColor:"lightblue",  alignItems: "center",fontSize:"20px"}}>My Gym Goals</h3>
+     <ul style={{backgroundColor:"lightblue", alignItems: "center",fontSize:"20px"}}>{listItems}</ul>
+  
     </div>
     );
 
